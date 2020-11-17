7a1461f92b5b401b9b422dd006db3d0dea3511e8
jerenz pellina <pellina01@gmail.com>
2020-11-17 17:13:05 +0800

    initial commit for i2c

--- i2c_arduino_mod.py
+++ i2c_arduino_mod.py
@@ -1,22 +1,20 @@
 import smbus2 as smbus
 
-# sensor type 1 fpr ph , 2 for turbidity
+# sensor type 1 for ph , 2 for turbidity
 
 
-def convert_strings_to_bytes(src):
-    converted = bytes(src, "utf-8")
+def convert_bytes_to_list(src):
     convert = []
-    for byte in converted:
+    for byte in src:
         convert.append(byte)
     return convert
 
 
 def read_arduino(slave_addr, sensor_type):
     I2Cbus = smbus.SMBus(1)
-    # i2c_slave_address = convert_strings_to_bytes(str(slave_addr))
     i2c_slave_address = slave_addr
-    byte = convert_strings_to_bytes(str(sensor_type))
+    byte = convert_bytes_to_list(bytes(str(sensor_type), "utf-8"))
     I2Cbus.write_i2c_block_data(i2c_slave_address, 0x00, byte)
     response = I2Cbus.read_i2c_block_data(i2c_slave_address, 0x00, 5)
-    res = convert_strings_to_bytes(response)
+    res = convert_bytes_to_list(response)
     return(res)
