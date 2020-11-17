ddd17d3c196c88dd5f5190a465c9f39fbf4a190f
jerenz pellina <pellina01@gmail.com>
2020-11-17 21:23:38 +0800

    removed excess code

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import i2c_arduino_mod as i2c
 import time
+import unixtime_api as clock
 
 
 # sensor type 1 fpr ph , 2 for turbidity
 while True:
     try:
         value = i2c.read_arduino(11, 1)
-        print(value)
-        print("end")
+        current_time = clock.getnow()
+        print(current_time + " " + value)
         time.sleep(2)
     except:
         print("error occured")
---
+++ unixtime_api.py
@@ -0,0 +1,9 @@
+import requests
+
+url = "http://worldtimeapi.org/api/timezone/asia/manila"
+
+
+def getnow():
+    json_response = requests.get(url=url)
+    data = json_response.json()
+    return(data["unixtime"])
