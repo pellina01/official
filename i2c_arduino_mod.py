c07847f8a96fcac566f68a6d17b8837153a29bea
jerenz pellina <pellina01@gmail.com>
2020-11-17 16:58:47 +0800

    initial commit for i2c

--- i2c_arduino_mod.py
+++ i2c_arduino_mod.py
@@ -13,7 +13,7 @@ import smbus2 as smbus
 
 def read_arduino(slave_addr, sensor_type):
     I2Cbus = smbus.SMBus(1)
-    i2c_slave_address = int(slave_addr)
+    i2c_slave_address = slave_addr
     # byte = convert_strings_to_bytes(sensor_type)
     I2Cbus.write_i2c_block_data(i2c_slave_address, 0x00, sensor_type)
     response = I2Cbus.read_i2c_block_data(i2c_slave_address, 0x00, 5)
--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import i2c_arduino_mod as i2c
 
 
 # sensor type 1 fpr ph , 2 for turbidity
-value = i2c.read_arduino(1, 1)
+value = i2c.read_arduino(11, 1)
 print(value)
