e594263710b20d93f3910a7fa95c89caf21732c2
jerenz pellina <pellina01@gmail.com>
2020-11-17 20:43:32 +0800

    removed excess code

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import i2c_arduino_mod as i2c
+import time
 
 
 # sensor type 1 fpr ph , 2 for turbidity
-value = i2c.read_arduino(11, 1)
-print(value)
-print("end")
+while True:
+    try:
+        value = i2c.read_arduino(11, 1)
+        print(value)
+        print("end")
+        time.sleep(2)
+    except:
+        print("error occured")
+        time.sleep(2)
