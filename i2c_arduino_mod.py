a69080737f166bdaf0347ca7f2e2ed64453f429d
jerenz pellina <pellina01@gmail.com>
2020-11-18 12:04:41 +0800

    mqtt added

--- i2c_arduino_mod.py
+++ i2c_arduino_mod.py
@@ -14,6 +14,6 @@ def read_arduino(slave_addr, sensor_type):
     I2Cbus = smbus.SMBus(1)
     byte = convert_bytes_to_list(bytes(str(sensor_type), "utf-8"))
     I2Cbus.write_i2c_block_data(slave_addr, 0x00, byte)
-    response = I2Cbus.read_i2c_block_data(slave_addr, 0x00, 5)
+    response = I2Cbus.read_i2c_block_data(slave_addr, 0x00, 25)
     res = bytearray(response).decode("utf-8", "ignore")
     return(res)
