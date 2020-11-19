352d4df045231e7f73c3dd0fd4d12df27f5f0cbc
jerenz pellina <pellina01@gmail.com>
2020-11-19 09:09:54 +0800

    json added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import i2c_arduino_mod as i2c
 import time
+import json
 import unixtime_api as clock
 from mqtt import mqtt
 from config import config
@@ -13,10 +14,10 @@ ph_mqtt = mqtt(topic, mqtt_url)
 # sensor type 1 fpr ph, 2 for turbidity
 while True:
     try:
-        value = i2c.read_arduino(11, 1)
-        current_time = clock.getnow(time_url, list_name)
-        to_send = str(value) + " at " + str(current_time)
-        ph_mqtt.send(to_send)
+        value = str(i2c.read_arduino(11, 1))
+        current_time = str(clock.getnow(time_url, list_name))
+        data = {"time": current_time, "value": value}
+        ph_mqtt.send(json.dumps(data))
         time.sleep(2)
     except Exception as e:
         print("error occured: ")
