b4d7865f509c21f99d52fea0ecd9189257c18c08
jerenz pellina <pellina01@gmail.com>
2020-11-19 09:16:31 +0800

    remove pycache folder

--- mqtt.py
+++ mqtt.py
@@ -6,7 +6,7 @@ class mqtt:
         self.topic = topic
         self.client = mqtt.Client()
         self.client.will_set(
-            self.topic, payload="Connection lost", qos=0, retain=False)
+            self.topic, payload="CONNECTION LOST", qos=0, retain=False)
         self.client.connect(url, 1883, 60)
         self.client.publish(self.topic, "CONNECTION OK")
         self.client.loop_start()
