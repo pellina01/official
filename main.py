bcc1dbc8a0844c424ff67818008feadcb257239e
jerenz pellina <pellina01@gmail.com>
2020-11-19 08:10:44 +0800

    make mqtt func as obj

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-import i2c_arduino_mod as i2c
+# import i2c_arduino_mod as i2c
 import time
 import unixtime_api as clock
-import mqtt
+from mqtt import mqtt
 
 topic = "topic/ph"
 url = "ec2-18-206-177-119.compute-1.amazonaws.com"
 
 ph_mqtt = mqtt(topic, url)
 
-# sensor type 1 fpr ph , 2 for turbidity
+# sensor type 1 fpr ph, 2 for turbidity
 while True:
     try:
         value = i2c.read_arduino(11, 1)
         current_time = clock.getnow()
         to_send = str(value) + " at " + str(current_time)
-        ph_mqtt.send(to_send)
+        ph_mqtt.send(current_time)
         time.sleep(2)
     except Exception as e:
         print("error occured: ")
--- mqtt.py
+++ mqtt.py
@@ -1,17 +1,16 @@
-import paho.mqtt.client as mqtt
 
-
-# "topic/test",
 class mqtt:
 
     def __init__(self, topic, url):
+        import paho.mqtt.client as mqtt
         self.topic = topic
         self.client = mqtt.Client()
-        client.will_set(
+        self.client.will_set(
             self.topic, payload="Connection lost", qos=0, retain=False)
-        client.connect(url, 1883, 60)
-        client.publish(self.topic, "CONNECTION OK")
-        client.loop_start()
+        self.client.connect(url, 1883, 60)
+        self.client.publish(self.topic, "CONNECTION OK")
+        self.client.loop_start()
+        print("done mqtt setup")
 
     def send(self, value):
-        client.publish(self.topic, value, retain=False)
+        self.client.publish(self.topic, value, retain=False)
