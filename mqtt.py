2057b8bf5427180a84a15a93b21cdd170086f346
jerenz pellina <pellina01@gmail.com>
2020-11-22 20:53:28 +0800

    added mqtt reconnect code

--- mqtt.py
+++ mqtt.py
@@ -4,11 +4,24 @@ class mqtt:
     def __init__(self, topic, url):
         import paho.mqtt.client as mqtt
         import json
+        import time
+        self.connected = False
         self.topic = topic
         self.client = mqtt.Client()
         self.client.will_set(
             self.topic, payload=json.dumps({"status": "disconnected"}), qos=0, retain=False)
-        self.client.connect(url, 1883, 60)
+
+        # run code until connect
+        self.printed = False
+        while self.connected is False:
+            try:
+                self.client.connect(url, 1883, 60)
+                self.connected = True
+            except:
+                if self.printed is False:
+                    print("failed to connect to %s, retrying...." % url)
+                    self.printed = True
+
         self.client.publish(self.topic, json.dumps({"status": "connected"}))
         self.client.loop_start()
         print("done '%s' mqtt setup" % self.topic)
