c7510742b13fde89cf6ef117b1927886a4de4ea9
jerenz pellina <pellina01@gmail.com>
2020-11-22 17:50:32 +0800

    added temp module

--- main.py
+++ main.py
@@ -4,20 +4,40 @@ import json
 import unixtime_api as clock
 from mqtt import mqtt
 from config import config
+import temp as w1temp
+#import datetime
 
-topic = config.topic
+
 mqtt_url = config.mqtt_url
 time_url = config.time_url
 list_name = config.list_name
 
-ph_mqtt = mqtt(topic, mqtt_url)
+
+ph = config.ph_topic
+tb = config.tb_topic
+temp = config.temp_topic
+
+ph_mqtt = mqtt(ph, mqtt_url)
+tb_mqtt = mqtt(tb, mqtt_url)
+temp_mqtt = mqtt(temp, mqtt_url)
+
 # sensor type 1 fpr ph, 2 for turbidity
 while True:
     try:
-        value = str(i2c.read_arduino(11, 1))
+        ph_value = str(i2c.read_arduino(11, 1))
+        tb_value = str(i2c.read_arduino(11, 2))
+        temp_value = str(w1temp.read_value())
+
         current_time = str(clock.getnow(time_url, list_name))
-        data = {"time": current_time, "value": value}
-        ph_mqtt.send(json.dumps(data))
+        ph_data = {"status": "connected",
+                   "time": current_time, "value": ph_value}
+        tb_data = {"status": "connected",
+                   "time": current_time, "value": tb_value}
+        temp_data = {"status": "connected",
+                     "time": current_time, "value": temp_value}
+        ph_mqtt.send(json.dumps(ph_data))
+        tb_mqtt.send(json.dumps(tb_data))
+        temp_mqtt.send(json.dumps(temp_data))
         time.sleep(2)
     except Exception as e:
         print("error occured: ")
--- mqtt.py
+++ mqtt.py
@@ -3,12 +3,13 @@ class mqtt:
 
     def __init__(self, topic, url):
         import paho.mqtt.client as mqtt
+        import json
         self.topic = topic
         self.client = mqtt.Client()
         self.client.will_set(
-            self.topic, payload="CONNECTION LOST", qos=0, retain=False)
+            self.topic, payload=json.dumps({"status": "disconnected"}), qos=0, retain=False)
         self.client.connect(url, 1883, 60)
-        self.client.publish(self.topic, "CONNECTION OK")
+        self.client.publish(self.topic, json.dumps({"status": "connected"}))
         self.client.loop_start()
         print("done '%s' mqtt setup" % self.topic)
 
