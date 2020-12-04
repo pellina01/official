72635d680dd17519e1116447cd47cbbca4206742
jerenz pellina <pellina01@gmail.com>
2020-12-04 11:21:16 +0800

    remove walrus operator for python 3.7

--- unixtime_api.py
+++ unixtime_api.py
@@ -4,15 +4,18 @@ import time
 
 def get_time(url, unix_name, url2, unix_name2):
     def getnow():
-        if requests.get(url=url).status_code == 200:
+        json_response = requests.get(url=url)
+        if json_response.status_code == 200:
             data = json_response.json()
             del json_response
             return data[unix_name]
-        elif requests.get(url=url2).status_code == 200:
-            data = json_response.json()
-            del json_response
-            return data[unix_name2]
         else:
-            print("API call failed. Substituting system time")
-            return int(time.time())
+            json_response = requests.get(url=url2)
+            if json_response.status_code == 200:
+                data = json_response.json()
+                del json_response
+                return data[unix_name2]
+            else:
+                print("API call failed. Substituting system time")
+                return int(time.time())
     return getnow
