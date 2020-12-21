65cf02c0ed98ff7c7d0ba4d1882af06c27f0de46
jerenz pellina <pellina01@gmail.com>
2020-12-21 19:58:35 +0800

    added pika to main.py

--- main.py
+++ main.py
@@ -7,56 +7,100 @@ import time
 import json
 import logging
 import traceback
-
-# sample comment
-with open('config.json', 'r') as file:
-    data = json.loads(file.read())
-
-raspi = {}
-for key, value in data["raspi"].items():
-    raspi.update({key: value})
-
-logging.basicConfig(filename=raspi["error_file"])
-
-ph_mqtt = mqtt(raspi["ph_topic"], raspi["mqtt_url"])
-tb_mqtt = mqtt(raspi["tb_topic"], raspi["mqtt_url"])
-temp_mqtt = mqtt(raspi["temp_topic"], raspi["mqtt_url"])
+import pika
+import os
+import datetime
 
 
-def formatter(value, topic):
-    print({"topic": topic, "status": "sending", "value": str(value)})
-    return json.dumps({"status": "sending", "value": str(value)})
+def internet_on():
+    if os.system("sudo ping -c 1 " + raspi["mqtt_url"]) == 0:
+        return True
+    else:
+        return False
 
 
-def mqtt_sensor(f1, f2, f3, topic, *arg):
+def formatter(value, topic, conn):
+    if conn:
+        print({"topic": topic, "status": "sending", "value": str(value)})
+        return json.dumps({"status": "sending", "value": str(value)})
+    else:
+        print({"topic": topic, "value": str(value),
+               "time": str(datetime.datetime.now())})
+        return json.dumps({"topic": topic, "value": str(value),
+                           "time": str(datetime.datetime.now())})
+
+
+def sensor_serializer(f1, f2, f3, topic, *arg):
     def get():
         try:
-            f1(f2(f3(arg[0], arg[1]), topic))
+            data = f3(arg[0], arg[1])
+            if internet_on():
+                f1(f2(data, topic, True))
+            else:
+                rabbit(f2(data, topic, False))
         except Exception:
-            print("error occured: %s" % traceback.format_exc())
-            print("error at topic: %s" % topic)
+            print('error occured: %s at topic %d' %
+                  (traceback.format_exc(), topic))
             logging.error(traceback.format_exc())
+        finally:
+            del data
     return(get)
 
 
-ph_send = mqtt_sensor(ph_mqtt.send, formatter, read_arduino, "ph", 11, 1)
-tb_send = mqtt_sensor(tb_mqtt.send, formatter, read_arduino, "tb", 11, 2)
-temp_send = mqtt_sensor(temp_mqtt.send, formatter, read_value, "temp", 0, 0)
+# rabbitmq client
+def rabbit(json_msg):
+    channel.basic_publish(
+        exchange='',
+        routing_key='task_queue',
+        body=json_msg,
+        properties=pika.BasicProperties(
+            delivery_mode=2,  # make message persistent
+        ))
+    print(" [x] Sent to queue %s" % json_msg)
 
-while True:
-    ph_send()
-    tb_send()
-    temp_send()
-    time.sleep(30)
 
-# while True:
-#     try:
-#         ph_mqtt.send(formatter(read_arduino(11, 1), "ph"))
-#         tb_mqtt.send(formatter(read_arduino(11, 2), "tb"))
-#         temp_mqtt.send(formatter(read_value(), "temp"))
-#         time.sleep(30)
-#     except Exception as e:
-#         print("error occured: %s" % traceback.format_exc())
-#         print("error message: %s" % e)
-#         logging.error(traceback.format_exc())
-#         time.sleep(2)
+if __name__ == "__main__":
+    time.sleep(300)
+    conn = internet_on()
+    is_printed = False
+    while conn is False:
+        conn = internet_on()
+        if is_printed is False:
+            print("cant reach the cloud server. retrying.....")
+            is_printed = True
+        time.sleep(3)
+
+    del is_printed
+    del conn
+    print("cloud server has been reached")
+
+    with open('config.json', 'r') as file:
+        data = json.loads(file.read())
+
+    raspi = {}
+    for key, value in data["raspi"].items():
+        raspi.update({key: value})
+
+    logging.basicConfig(filename=raspi["error_file"])
+
+    ph_mqtt = mqtt(raspi["ph_topic"], raspi["mqtt_url"])
+    tb_mqtt = mqtt(raspi["tb_topic"], raspi["mqtt_url"])
+    temp_mqtt = mqtt(raspi["temp_topic"], raspi["mqtt_url"])
+
+    # rabbitmq object
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=raspi["mqtt_url"]))
+    channel = connection.channel()
+    channel.queue_declare(queue='task_queue', durable=True)
+
+    ph_send = sensor_serializer(
+        ph_mqtt.send, formatter, read_arduino, "ph", 11, 1)
+    tb_send = sensor_serializer(
+        tb_mqtt.send, formatter, read_arduino, "tb", 11, 2)
+    temp_send = sensor_serializer(temp_mqtt.send, formatter,
+                                  read_value, "temp", 0, 0)
+    while True:
+        ph_send()
+        tb_send()
+        temp_send()
+        time.sleep(30)
