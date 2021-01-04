280f8fa6aad438eac9aac8c38e8ec79dc6bd69bc
jerenz pellina <pellina01@gmail.com>
2021-01-04 11:59:21 +0800

    refactor mainv2

--- sensor_serializer.py
+++ sensor_serializer.py
@@ -4,17 +4,22 @@ class serializer:
     from do import read_do
     import logging
     import traceback
+    import json
 
     def __formatter(self, value, topic):
         print({"topic": topic, "status": "sending", "value": str(value)})
-        return self.json.dumps({"status": "sending", "value": str(value)})
+        return json.dumps({"status": "sending", "value": str(value)})
+
+    def __serialize(self, mqtt_send, sensor_function, topic, slave_addr, sensor_type):
+        def get_then_send():
+            mqtt_send(self.__formatter(
+                sensor_function(slave_addr, sensor_type), topic))
+        return(get_then_send)
 
     def __init__(self, url, sensor):
-        import json
         from mqtt import mqtt
 
         self.logging.basicConfig(filename="error.log")
-        self.json = json
 
         sensor_type = sensor[3]
         slave_addr = sensor[2]
@@ -25,11 +30,12 @@ class serializer:
             "read_value": read_value,
             "read_do": read_do
         }
-        self.get_send = mqtt(topic, url).send(
-            self.__formatter(
-                switch.get(sensor_function)(slave_addr, sensor_type),
-                topic
-            )
+        self.get_send = self.__serialize(
+            mqtt(topic, url).send,
+            switch.get(sensor_function),
+            topic,
+            slave_addr,
+            sensor_type
         )
 
     def process(self):
